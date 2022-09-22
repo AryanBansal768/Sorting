2315b0c6e4b0172f00fab0ca52c34c0aa9ec9030
Aryan Bansal <Aryan Bansal@LAPTOP-HKERMFJ4>
2022-09-22 14:42:43 +0530

    New Sort

---
+++ src/NewSort.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class NewSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {13,8,66,4,47,55,105,455};
+		Arrays.sort(arr);
+		System.out.printf("Modified arr[] : %s",Arrays.toString(arr));
+	}
+
+}
